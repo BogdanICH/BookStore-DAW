f8745411d471abecce3127976c7c7c5482a305e7
Alexandru Nastase <nalexandru27@yahoo.com>
2020-10-29 19:53:05 +0200

    Adding MVC classes Connection to Database Define Route

--- BookStore/App_Start/RouteConfig.cs
+++ BookStore/App_Start/RouteConfig.cs
@@ -16,8 +16,9 @@ namespace BookStore
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Books", action = "Index", id = UrlParameter.Optional }
             );
+            //TODO: adaugare ruta /Books/Details/{id}
         }
     }
 }
---
+++ BookStore/Data Access Layer/DbCtx.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Data_Access_Layer
+{
+    public class DbCtx : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<ContactInfo> ContactInfo { get; set; }
+        public DbCtx() : base("BookCS")
+        {
+            Database.SetInitializer<DbCtx>(new Initp());
+        }
+    }
+}
